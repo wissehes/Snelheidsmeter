0cfca25d52937e5c4a34881c0889662619687281
Wisse <wissehes@gmail.com>
2023-09-20 20:45:21 +0200

    Create main UI

--- Snelheidsmeter/ContentView.swift
+++ Snelheidsmeter/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var vm = SpeedViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .center, spacing: 10) {
+            SpeedView(speed: vm.speed)
+            
+            Button(vm.isMonitoring ? "Stop volgen" : "Start volgen", systemImage: "location.fill.viewfinder") {
+                if vm.isMonitoring {
+                    vm.locationMonitoringTask?.cancel()
+                } else {
+                    vm.locationMonitoringTask = Task { await vm.startMonitoring() }
+                }
+            }.buttonStyle(.borderedProminent)
         }
         .padding()
     }
--- Snelheidsmeter/SpeedViewModel.swift
+++ Snelheidsmeter/SpeedViewModel.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+import SwiftUI
+import CoreLocation
+import AsyncLocationKit
+import CoreMotion
+
+@Observable
+class SpeedViewModel {
+    
+    private let manager = AsyncLocationManager(desiredAccuracy: .bestForNavigationAccuracy)
+    
+    var location: CLLocation?
+    var isMonitoring: Bool = false
+    
+    var speed: Double {
+        guard let speed = location?.speed else { return 0 }
+        
+        if speed < 0 {
+            return 0
+        } else {
+            return speed
+        }
+    }
+    
+    var locationMonitoringTask: Task<Void, Never>?
+    
+    func setMonitoring(_ value: Bool) {
+        withAnimation { self.isMonitoring = value }
+    }
+    
+    @MainActor
+    func startMonitoring() async {
+        defer {
+            self.setMonitoring(false);
+            self.location = nil
+        }
+        
+        print("Starting...")
+        
+        let permission = await manager.requestPermission(with: .whenInUsage)
+        
+        guard permission == .authorizedWhenInUse || permission == .authorizedAlways else { return print("Denied location") }
+        
+        self.setMonitoring(true)
+        
+        for await locationUpdate in await manager.startUpdatingLocation() {
+            
+            switch locationUpdate {
+            case .didUpdateLocations(let locations):
+                withAnimation { self.location = locations.last }
+            case .didPaused:
+                setMonitoring(false)
+            case .didResume:
+                setMonitoring(true)
+            case .didFailWith(error: let error):
+                print(error)
+            }
+        }
+        
+    }
+}

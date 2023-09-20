c992ec3e27a6edb1551f9f8d73d9461c7ca9208a
Wisse <wissehes@gmail.com>
2023-09-20 22:12:28 +0200

    Calculate acceleration

--- Snelheidsmeter/AccelerationView.swift
+++ Snelheidsmeter/AccelerationView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct AccelerationView: View {
+    
+    var acceleration: Double?
+    
+    let gradient = Gradient(colors: [.green, .orange, .pink])
+    let minAccel: Double = -10
+    let maxAccel: Double = 10
+    
+    var formatter: MeasurementFormatter {
+        let formatter = MeasurementFormatter()
+        formatter.numberFormatter.numberStyle = .decimal
+        formatter.numberFormatter.minimumFractionDigits = 2
+        return formatter
+    }
+    
+    var formattedAcceleration: String {
+        guard let acceleration = acceleration else { return "?" }
+        let measurement = Measurement(value: acceleration, unit: UnitAcceleration.metersPerSecondSquared)
+        return formatter.string(from: measurement)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 12.5) {
+            VStack {
+                Label("Versnelling", systemImage: "speedometer")
+                    .font(.headline)
+                Text(formattedAcceleration)
+                    .font(.system(.largeTitle, design: .rounded))
+            }
+            
+            Gauge(value: acceleration ?? 0, in: minAccel...maxAccel) {
+                Text("Versnelling")
+            } currentValueLabel: {
+                Text(formattedAcceleration)
+            } minimumValueLabel:  {
+                Text(Int(minAccel).description)
+            } maximumValueLabel: {
+                Text(Int(maxAccel).description)
+            }.labelsHidden()
+                .gaugeStyle(.accessoryLinear)
+                .tint(gradient)
+        }.padding()
+            .frame(height: 150)
     }
 }
 
 #Preview {
-    AccelerationView()
+    AccelerationView(acceleration: 2.4)
 }

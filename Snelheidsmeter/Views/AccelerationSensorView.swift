9fd160272b4997b553b5777e1cd571749556fd3b
Wisse <wissehes@gmail.com>
2023-09-20 22:25:07 +0200

    Move files to Views folder

---
+++ Snelheidsmeter/Views/AccelerationSensorView.swift
@@ -0,0 +1,36 @@
+//
+//  AccelerationSensorView.swift
+//  Snelheidsmeter
+//
+//  Created by Wisse Hes on 20/09/2023.
+//
+
+import SwiftUI
+import CoreMotion
+
+struct AccelerationSensorView: View {
+    
+    let manager = CMMotionManager()
+    
+    @State private var acceleration: CMAcceleration?
+    
+    var body: some View {
+        VStack {
+            Text("Acceleration:")
+            Text("X: \(acceleration?.x.formatted() ?? "?")")
+            Text("Y: \(acceleration?.y.formatted() ?? "?")")
+            Text("Z: \(acceleration?.z.formatted() ?? "?")")
+        }
+            .onAppear {
+                manager.deviceMotionUpdateInterval = 1
+                manager.startDeviceMotionUpdates(to: .current!) { motion, error in
+                    print(error, motion)
+                    self.acceleration = motion?.userAcceleration
+                }
+            }
+    }
+}
+
+#Preview {
+    AccelerationSensorView()
+}

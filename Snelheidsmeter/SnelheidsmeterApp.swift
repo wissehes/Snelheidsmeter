85f638bec5b286cedc4fb69a171fc02afed0d1f9
Wisse <wissehes@gmail.com>
2023-09-20 17:38:00 +0200

    Initial Commit

---
+++ Snelheidsmeter/SnelheidsmeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnelheidsmeterApp.swift
+//  Snelheidsmeter
+//
+//  Created by Wisse Hes on 20/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnelheidsmeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

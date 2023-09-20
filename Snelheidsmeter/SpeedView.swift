76d3b2ea22db69a82ef73678084a243f80406b1a
Wisse <wissehes@gmail.com>
2023-09-20 20:04:22 +0200

    Set up project and packages

---
+++ Snelheidsmeter/SpeedView.swift
@@ -0,0 +1,18 @@
+//
+//  SpeedView.swift
+//  Snelheidsmeter
+//
+//  Created by Wisse Hes on 20/09/2023.
+//
+
+import SwiftUI
+
+struct SpeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpeedView()
+}
---
+++ Snelheidsmeter/SpeedViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SpeedViewModel.swift
+//  Snelheidsmeter
+//
+//  Created by Wisse Hes on 20/09/2023.
+//
+
+import Foundation

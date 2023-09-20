4e4795f6c6014641123a5fc15838941d260cba54
Wisse <wissehes@gmail.com>
2023-09-20 22:12:00 +0200

    Add AccelerationSensorView for use in the future

---
+++ Snelheidsmeter/AccelerationView.swift
@@ -0,0 +1,18 @@
+//
+//  AccelerationView.swift
+//  Snelheidsmeter
+//
+//  Created by Wisse Hes on 20/09/2023.
+//
+
+import SwiftUI
+
+struct AccelerationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccelerationView()
+}

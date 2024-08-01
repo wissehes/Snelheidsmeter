ec0c5e0ab16ce49625972885af1e55b2f5b7e5ed
Wisse Hes <wisse@wissehes.nl>
2024-08-01 09:05:54 +0200

    Add keys to the defaults

--- Snelheidsmeter/Defaults.swift
+++ Snelheidsmeter/Defaults.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import Defaults
+
+extension Defaults.Keys {
+    /// Whether the app should start tracking when the app is opened
+    static let startOnOpen = Key<Bool>("start-on-open", default: false)
+    
+    /// If the app should show the acceleration gauge
+    static let showAcceleration = Key<Bool>("show-acceleration", default: true)
+}
